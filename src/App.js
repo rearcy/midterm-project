293df77e0029ecbd6da2fea2521f7eae30079d06
RPearcy <techspace@Risas-MacBook-Air.local>
2024-01-20 20:08:34 -0900

    added states

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import React from 'react';
+import GuessingGame from './GuessingGame';
 
-function App() {
+function App(){
+
+
+
   return (
-    <div style={{ textAlign: 'center' }}>
-      <header>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+    
+<GuessingGame/>
+   
+  )
+} 
 
 export default App;
